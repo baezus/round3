4c8dd752b748df133f22266827f7804d227469f9
baezus <elias@baez.us>
2022-10-06 22:53:08 -0400

    Sloppy but improved.

--- Main.hs
+++ Main.hs
@@ -11,12 +11,35 @@ main :: IO ()
 main = do
     let rp = "reflex-platform/"
     let n = "/n"
+    currentDir <- getCurrentDirectory 
+    print currentDir 
     createDirectory rp
+    let z = aeU [currentDir, rp]
+    print z 
+    let x = aeU [rp, "github.json"]
+    let y = aeU [rp, "default.nix"]
+    let b = aeU [rp, "thunk.nix"]
+    -- setCurrentDirectory z
+    writeFile x n 
+    writeFile y n 
+    writeFile b n 
+    setCurrentDirectory currentDir  
     writeFile "default.nix" n 
-    writeFile "shell.nix" n
-    writeFile "reflex-platform/default.nix" n
-    writeFile "reflex-platform/github.json" n
-    writeFile "reflex-platform/thunk.nix" n
+    writeFile "shell.nix" n 
+    -- check12 <- getCurrentDirectory
+    -- print check12 
+    -- withCurrentDirectory currentDir $ writeFile x n
+    -- writeFile "default.nix" n 
+    -- writeFile "shell.nix" n
+    -- writeFile "reflex-platform/default.nix" n
+    -- writeFile x n
+    -- writeFile "reflex-platform/thunk.nix" n
 
 aeU :: [FilePath] -> FilePath
 aeU = joinPath
+
+-- create directory
+-- set to reflex-platform directory 
+-- create those entries
+-- pop up to normal directory again
+-- create last two entries 
